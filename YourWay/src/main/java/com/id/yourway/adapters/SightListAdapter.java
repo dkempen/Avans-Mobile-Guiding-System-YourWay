d0e39d434f9c6f8f3641dc5000efecc7e7200bfe
Ralph <rasrouwe@avans.nl>
2018-12-10 13:10:28 +0100

    [added] sight list

--- YourWay/src/main/java/com/id/yourway/adapters/SightListAdapter.java
+++ YourWay/src/main/java/com/id/yourway/adapters/SightListAdapter.java
@@ -1,4 +1,69 @@
 package com.id.yourway.adapters;
 
-public class SightListAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.id.yourway.R;
+import com.id.yourway.entities.Sight;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SightListAdapter extends RecyclerView.Adapter<SightListAdapter.SightViewHolder>{
+
+    private List<Sight> sights;
+    private Context context;
+
+    public SightListAdapter(Context context, List<Sight> sights) {
+        this.context = context;
+        this.sights = sights;
+    }
+
+    @Override
+    public int getItemCount() {
+        return sights.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull final SightViewHolder viewHolder, int position) {
+        Sight sight = sights.get(position);
+        viewHolder.nameTextView.setText(sight.getAuthor());
+    }
+
+    @NonNull
+    @Override
+    public SightViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.item_sight, parent, false);
+
+        return new SightViewHolder(v, context);
+    }
+
+    public class SightViewHolder extends RecyclerView.ViewHolder {
+
+        TextView nameTextView;
+        ImageView imageView;
+
+        public SightViewHolder(View itemView, final Context ctx) {
+            super(itemView);
+            context = ctx;
+
+            nameTextView = itemView.findViewById(R.id.item_sight_name_id);
+            imageView = itemView.findViewById(R.id.item_sight_imageView);
+
+            //OnClick listener voor cardview item.
+//            itemView.setOnClickListener((View v) -> {
+//                Light light2 = lights.get(getAdapterPosition());
+//                Intent intent = new Intent(context, LightDetailedActivity.class);
+//                ctx.startActivity(intent);
+//            });
+        }
+    }
 }
--- YourWay/src/main/java/com/id/yourway/providers/GoogleDirectionsProvider.java
+++ YourWay/src/main/java/com/id/yourway/providers/GoogleDirectionsProvider.java
@@ -10,29 +10,29 @@ import org.json.JSONObject;
 import java.net.MalformedURLException;
 import java.net.URL;
 
-public class GoogleDirectionsProvider implements DirectionsProvider {
-
-    private static String BASE_URL  = "";
-    private String apikey;
-    private RestProvider restProvider;
-
-    public GoogleDirectionsProvider(Context context) {
-        restProvider = RestProvider.getInstance(context);
-
-    }
-
-    @Override
-    public void queueDirectionsRequest(DirectionsProviderListener listener) {
-            restProvider.getRequest(BASE_URL + "", new RestProviderListener() {
-                @Override
-                public void onRequestObjectAvailible(JSONObject response, RestResponseType restResponseType) {
-
-                }
-
-                @Override
-                public void onRequestError(VolleyError error, RestResponseType restResponseType) {
-
-                }
-            },);
-    }
-}
+//public class GoogleDirectionsProvider implements DirectionsProvider {
+//
+//    private static String BASE_URL  = "";
+//    private String apikey;
+//    private RestProvider restProvider;
+//
+//    public GoogleDirectionsProvider(Context context) {
+//        restProvider = RestProvider.getInstance(context);
+//
+//    }
+//
+////    @Override
+////    public void queueDirectionsRequest(DirectionsProviderListener listener) {
+////            restProvider.getRequest(BASE_URL + "", new RestProviderListener() {
+////                @Override
+////                public void onRequestObjectAvailible(JSONObject response, RestResponseType restResponseType) {
+////
+////                }
+////
+////                @Override
+////                public void onRequestError(VolleyError error, RestResponseType restResponseType) {
+////
+////                }
+////            },);
+////    }
+//}
