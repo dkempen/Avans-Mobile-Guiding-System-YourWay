178d8d10c299f1a6bcfe7d16e6a4c62692b1b7a3
Ralph <rasrouwe@avans.nl>
2018-12-10 13:19:06 +0100

    [added] startup detailled

--- YourWay/src/main/java/com/id/yourway/activities/DetailActivity.java
+++ YourWay/src/main/java/com/id/yourway/activities/DetailActivity.java
@@ -2,14 +2,26 @@ package com.id.yourway.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.id.yourway.R;
 
 public class DetailActivity extends AppCompatActivity {
 
+    TextView muralAuthorName;
+    TextView muralDescription;
+    TextView muralYear;
+    ImageView muralImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        muralAuthorName = findViewById(R.id.detailedActivity_muralName);
+        muralDescription = findViewById(R.id.detailedActivity_muralDescription);
+        muralYear = findViewById(R.id.detailedActivity_muralYear);
+        muralImage = findViewById(R.id.detailedActivity_muralImage);
     }
 }
