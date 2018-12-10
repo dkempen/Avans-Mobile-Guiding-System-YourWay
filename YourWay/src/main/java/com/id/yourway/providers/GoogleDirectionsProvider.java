dcb44534aaf2e3ffc1380323e9cf7e15067c58fa
private <chbcorne@avans.nl>
2018-12-10 11:56:19 +0100

    [Changed] method of queuing

--- YourWay/src/main/java/com/id/yourway/providers/GoogleDirectionsProvider.java
+++ YourWay/src/main/java/com/id/yourway/providers/GoogleDirectionsProvider.java
@@ -2,21 +2,40 @@ package com.id.yourway.providers;
 
 import android.content.Context;
 
+import com.id.yourway.providers.interfaces.DirectionsProvider;
+import com.android.volley.VolleyError;
 import com.id.yourway.R;
+import com.id.yourway.entities.RestResponseType;
 import com.id.yourway.providers.listeners.DirectionsProviderListener;
+
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class GoogleDirectionsProvider implements DirectionsProvider {
 
+    private static String BASE_URL  = "";
     private String apikey;
     private RestProvider restProvider;
 
     public GoogleDirectionsProvider(Context context) {
-        restProvider = new RestProvider();
-        //context.getResources().getString()
+        restProvider = RestProvider.getInstance(context);
+
     }
 
     @Override
     public void queueDirectionsRequest(DirectionsProviderListener listener) {
+            restProvider.getRequest(BASE_URL + "", new RestProviderListener() {
+                @Override
+                public void onRequestObjectAvailible(JSONObject response, RestResponseType restResponseType) {
 
+                }
+
+                @Override
+                public void onRequestError(VolleyError error, RestResponseType restResponseType) {
+
+                }
+            },);
     }
 }
