875023c175eb71abdf89293fab14e9c4a5093297
dkempen <daan.vankempen2@gmail.com>
2018-12-10 12:40:16 +0100

    [Changed] Rest to work with JsonArray + simplified Sight class

--- YourWay/src/main/java/com/id/yourway/entities/Sight.java
+++ YourWay/src/main/java/com/id/yourway/entities/Sight.java
@@ -8,22 +8,15 @@ public class Sight implements Comparable<Sight>, Serializable {
     private final static String TAG = Sight.class.getSimpleName();
 
     private int id;
-    private int published;
     private long date;
-    private long dateModified;
-    private int authorID;
     private double latitude;
     private double longitude;
     private String address;
-    private int numberOnMap;
     private String videoUrl;
-    private String year;
     private String photographer;
     private String author;
     private String titleNL;
     private String titleEN;
-    private String urlNL;
-    private String urlEN;
     private String descriptionNL;
     private String descriptionEN;
     private String materialNL;
@@ -32,24 +25,20 @@ public class Sight implements Comparable<Sight>, Serializable {
     private String catogoryEN;
     private List<String> images;
 
-    public Sight(int id, int published, long date, long dateModified, int authorID, double latitude, double longitude, String address, int numberOnMap, String videoUrl, String year, String photographer, String author, String titleNL, String titleEN, String urlNL, String urlEN, String descriptionNL, String descriptionEN, String materialNL, String materialEN, String catogoryNL, String catogoryEN, List<String> images) {
+    public Sight(int id, long date, double latitude, double longitude, String address,
+                 String videoUrl, String photographer, String author, String titleNL,
+                 String titleEN, String descriptionNL, String descriptionEN, String materialNL,
+                 String materialEN, String catogoryNL, String catogoryEN, List<String> images) {
         this.id = id;
-        this.published = published;
         this.date = date;
-        this.dateModified = dateModified;
-        this.authorID = authorID;
         this.latitude = latitude;
         this.longitude = longitude;
         this.address = address;
-        this.numberOnMap = numberOnMap;
         this.videoUrl = videoUrl;
-        this.year = year;
         this.photographer = photographer;
         this.author = author;
         this.titleNL = titleNL;
         this.titleEN = titleEN;
-        this.urlNL = urlNL;
-        this.urlEN = urlEN;
         this.descriptionNL = descriptionNL;
         this.descriptionEN = descriptionEN;
         this.materialNL = materialNL;
@@ -59,7 +48,7 @@ public class Sight implements Comparable<Sight>, Serializable {
         this.images = images;
     }
 
-    private String getvariable(String NL, String EN){
+    private String getVariable(String NL, String EN){
         if(Locale.getDefault().getLanguage().equals("nl"))
             return NL;
         return EN;
@@ -69,20 +58,8 @@ public class Sight implements Comparable<Sight>, Serializable {
         return id;
     }
 
-    public int getPublished() {
-        return published;
-    }
-
     public long getDate() {
         return date;
-    }
-
-    public long getDateModified() {
-        return dateModified;
-    }
-
-    public int getAuthorID() {
-        return authorID;
     }
 
     public double getLatitude() {
@@ -97,45 +74,32 @@ public class Sight implements Comparable<Sight>, Serializable {
         return address;
     }
 
-    public int getNumberOnMap() {
-        return numberOnMap;
-    }
-
     public String getVideoUrl() {
         return videoUrl;
-    }
-
-    public String getYear() {
-        return year;
     }
 
     public String getPhotographer() {
         return photographer;
     }
 
-
     public String getAuthor() {
         return author;
     }
 
     public String getTitle() {
-        return getvariable(titleNL, titleEN);
-    }
-
-    public String getUrl() {
-        return getvariable(urlNL, urlEN);
+        return getVariable(titleNL, titleEN);
     }
 
     public String getDescription() {
-        return getvariable(descriptionNL, descriptionEN);
+        return getVariable(descriptionNL, descriptionEN);
     }
 
     public String getMaterial() {
-        return getvariable(materialNL, materialEN);
+        return getVariable(materialNL, materialEN);
 }
 
-    public String getCatogoryNL() {
-        return getvariable(catogoryNL, catogoryEN);
+    public String getCatogory() {
+        return getVariable(catogoryNL, catogoryEN);
     }
 
     public List<String> getImages() {
@@ -146,20 +110,14 @@ public class Sight implements Comparable<Sight>, Serializable {
     public String toString() {
         return "BlindWall{" +
                 "id=" + id +
-                ", published=" + published +
                 ", date=" + date +
-                ", dateModified=" + dateModified +
-                ", authorID=" + authorID +
                 ", latitude=" + latitude +
                 ", longitude=" + longitude +
                 ", address='" + address + '\'' +
-                ", numberOnMap=" + numberOnMap +
                 ", videoUrl='" + videoUrl + '\'' +
-                ", year='" + year + '\'' +
                 ", photographer='" + photographer + '\'' +
                 ", author='" + author + '\'' +
                 ", title=" + getTitle() +
-                ", url=" + getUrl() +
                 ", description=" + getDescription() +
                 ", images=" + images.toString() +
                 '}';
@@ -169,5 +127,4 @@ public class Sight implements Comparable<Sight>, Serializable {
     public int compareTo(Sight o) {
         return 0;
     }
-
 }
