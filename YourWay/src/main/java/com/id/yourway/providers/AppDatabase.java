714779941071cddf550e366b74ee5fbe5eb4bbc2
private <chbcorne@avans.nl>
2018-12-12 00:34:01 +0100

    [Added] database v1

--- YourWay/src/main/java/com/id/yourway/providers/AppDatabase.java
+++ YourWay/src/main/java/com/id/yourway/providers/AppDatabase.java
@@ -1,4 +1,33 @@
 package com.id.yourway.providers;
 
-public class AppDatabase {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class AppDatabase extends SQLiteOpenHelper {
+
+
+    private static final String NAME= "appDatabase";
+    private static final int VERSION = 1;
+
+
+    public AppDatabase(Context context) {
+        super(context, NAME, null, VERSION);
+
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        //TODO add reflection for finding tables
+        db.execSQL(RouteDAO.fetchTableSignature());
+    }
+
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+       if(oldVersion < newVersion){
+           db.execSQL("DROP TABLE IF EXISTS " + RouteDAO.TABLE_NAME);
+           onCreate(db);
+       }
+    }
 }
