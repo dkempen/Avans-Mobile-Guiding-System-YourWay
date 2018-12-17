4fd413886a3922c96d46152d355d16646025ebd7
Unknown <bart.vanes1@gmail.com>
2018-12-17 13:26:33 +0100

    [Added]  Vibrator stopper

--- YourWay/src/main/java/com/id/yourway/activities/PreferencesActivity.java
+++ YourWay/src/main/java/com/id/yourway/activities/PreferencesActivity.java
@@ -1,7 +1,10 @@
 package com.id.yourway.activities;
 
+import android.app.NotificationManager;
 import android.content.Context;
+import android.content.Intent;
 import android.media.AudioManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.support.v7.app.AppCompatActivity;
@@ -21,14 +24,27 @@ public class PreferencesActivity extends AppCompatActivity {
         vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         Switch vibrateSwitch = findViewById(R.id.VibrateSwitch);
 
+        NotificationManager notificationManager =
+                (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !notificationManager.isNotificationPolicyAccessGranted()) {
+
+            Intent intent = new Intent(
+                    android.provider.Settings
+                            .ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+
+            startActivity(intent);
+        }
+
+
         vibrateSwitch.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 AudioManager aManager=(AudioManager)getSystemService(AUDIO_SERVICE);
                 if (vibrateSwitch.isEnabled()) {
-                    aManager.setRingerMode(aManager.RINGER_MODE_SILENT);
+                    aManager.setRingerMode(aManager.VIBRATE_SETTING_ONLY_SILENT);
                     System.out.println("nee");
-
                 }
             }
         });
